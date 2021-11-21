509fb58088b4245a4cf5f38b8a35cd2c9a0b7d56
darshan527 <darshan.hosamani6@gmail.com>
2021-11-22 00:34:07 +0530

    Modified Page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import Head from 'next/head'
-
+import Layout from '../../components/layout'
 const FirstPost = () => {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -11,7 +11,7 @@ const FirstPost = () => {
                 <h1>This is first Post.</h1>
                 <h2><Link href="/"><a>Return to Homepage</a></Link></h2>
             </div>
-        </>
+        </Layout>
     )
 }
 
