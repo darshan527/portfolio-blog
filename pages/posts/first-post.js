a8989912608e33641e04abe311c875afee0e2985
darshan527 <darshan.hosamani6@gmail.com>
2021-11-22 00:12:59 +0530

    Added Head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,17 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const FirstPost = () => {
     return (
-        <div>
-            <h1>This is first Post.</h1>
-            <h2><Link href="/"><a>Return to Homepage</a></Link></h2>
-        </div>
+        <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <div>
+                <h1>This is first Post.</h1>
+                <h2><Link href="/"><a>Return to Homepage</a></Link></h2>
+            </div>
+        </>
     )
 }
 
